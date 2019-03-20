033be4226bb0f0a39ff4a132186e7538707705ae
Salakar <mike.diarmid@gmail.com>
2019-03-20 22:32:56 +0000

    -

--- App.js
+++ App.js
@@ -8,21 +8,13 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import WebView from "react-native-webview";
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <WebView style={{flex: 1}} source={{uri: "https://facebook.com"}}/>
+        <Text>Look ma, no linking!</Text>
       </View>
     );
   }
